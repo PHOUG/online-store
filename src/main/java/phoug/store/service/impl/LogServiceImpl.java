9e02a19011cdca9912b3c5d1014b369c2fa674c3
phoug <golheus@gmail.com>
2025-05-01 13:27:37 +0300

    SonarCloud trouble fix

--- src/main/java/phoug/store/service/impl/LogServiceImpl.java
+++ src/main/java/phoug/store/service/impl/LogServiceImpl.java
@@ -20,7 +20,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
--- src/test/java/phoug/store/LogServiceImplTest.java
+++ src/test/java/phoug/store/LogServiceImplTest.java
@@ -3,7 +3,6 @@ package phoug.store;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
